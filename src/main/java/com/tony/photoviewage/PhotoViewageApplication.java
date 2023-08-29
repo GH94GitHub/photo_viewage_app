526076e95247991fa27d6a12fd46736ef224d8c5
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2023-08-29 16:10:44 -0400

    Added default greeting endpoint

--- src/main/java/com/tony/photoviewage/PhotoViewageApplication.java
+++ src/main/java/com/tony/photoviewage/PhotoViewageApplication.java
@@ -2,12 +2,19 @@ package com.tony.photoviewage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PhotoViewageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoViewageApplication.class, args);
 	}
 
+	@GetMapping()
+	public String hello() {
+		return "Hello from PhotoViewage";
+	}
 }
