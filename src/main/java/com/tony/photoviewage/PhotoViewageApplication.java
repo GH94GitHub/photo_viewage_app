3bdb79a796b0e066b226c38cdfb6b374fbc0b816
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2023-08-29 16:09:35 -0400

    Initial commit

---
+++ src/main/java/com/tony/photoviewage/PhotoViewageApplication.java
@@ -0,0 +1,13 @@
+package com.tony.photoviewage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoViewageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoViewageApplication.class, args);
+	}
+
+}
